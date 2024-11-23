94e135db2279ffdc09e072e07a8c1b262944c182
VitorSantana113 <vitor@t2s.com.br>
2024-11-23 01:28:03 -0300

    Implementações e modificações

--- fatour/home/pedidos/script.js
+++ fatour/home/pedidos/script.js
@@ -1,47 +1,21 @@
-const detalhesButton = document.querySelector('.detalhes');
-const modal = document.getElementById('modal');
-const closeButton = document.querySelector('.close-button');
+const container = document.querySelector('.containerPedidos');
 
-detalhesButton.addEventListener('click', () => {
-    modal.style.display = 'block';
+container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('detalhes')) {
+        const modal = document.getElementById('modal');
+        modal.style.display = 'block';
+    }
 });
 
+const closeButton = document.querySelector('.close-button');
 closeButton.addEventListener('click', () => {
+    const modal = document.getElementById('modal');
     modal.style.display = 'none';
 });
 
 window.addEventListener('click', (event) => {
+    const modal = document.getElementById('modal');
     if (event.target === modal) {
         modal.style.display = 'none';
-    }
-});
-
-document.getElementById('search-button').addEventListener('click', function () {
-    const searchValue = document.getElementById('search-bar').value.trim().toLowerCase();
-    const orders = document.querySelectorAll('.order');
-
-    let found = false;
-
-    orders.forEach(order => {
-        const orderCode = order.querySelector('h3, .product-code').textContent.toLowerCase();
-
-        if (orderCode.includes(searchValue)) {
-            order.classList.remove('highlight');
-
-            setTimeout(() => {
-                order.classList.add('highlight');
-            }, 10);
-            setTimeout(() => {
-                order.classList.remove('highlight');
-            }, 3000);
-
-            found = true;
-        } else {
-            order.classList.remove('highlight');
-        }
-    });
-
-    if (!found) {
-        alert('Nenhum pedido encontrado com esse código.');
     }
 });
